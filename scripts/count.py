167ebc3fccf1017949b0f4436eb0fe9a155fc266
~Ryosuke <~ryosuke.810.109@gmail.com~>
2022-01-13 06:49:36 +0000

    update

---
+++ scripts/count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#LICENSE:BSD
+#*Copyright (c) 2022 Ryuichi Ueda. All right reserved.
+
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node( 'count' )
+pub = rospy.Publisher( 'count_up', Int32, queue_size=1)
+rate = rospy.Rate(10)
+n = 0
+while not rospy.is_shutdown():
+    n += 1
+    pub.publish(n)
+    rate.sleep()
